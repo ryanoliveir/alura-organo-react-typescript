964c07a7342113732ffd7d3faee7e5d1dc9c871c
ryanoliveir <ryandehallo229@gmail.com>
2023-04-23 18:37:46 -0300

    :construction: Added components
    Banner and Input component added

---
+++ src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+const Banner = () =>{
+    return (
+        <header className="banner">
+            <img src="/images/Banner.png" alt="Page Banner"/>
+        </header>
+    )
+}
+
+
+export default Banner;
---
+++ src/components/Input/index.js
@@ -0,0 +1,13 @@
+import './Input.css';
+
+const Input = () => {
+    return (
+        <div>
+            <label>Nome</label>
+            <input />
+        </div>
+    )
+
+}
+
+export default Input;
